f6ad55bb30670ff03e01560ba51bf237dd3dd1fe
vji2i0 <vladislavmipt@gmail.com>
2020-08-28 21:50:59 +0300

    Off verbose mode
    Off verbose mode for all the tests

--- 3DPrinterV3/Menu/Menu_Constructor/objs/AllTests.d
+++ 3DPrinterV3/Menu/Menu_Constructor/objs/AllTests.d
@@ -1,52 +1,58 @@
 objs/./AllTests.o: AllTests.cpp \
- /home/vji2i0/TDD/cpputest-3.7.2/include/CppUTest/MemoryLeakDetectorNewMacros.h \
- /home/vji2i0/TDD/cpputest-3.7.2/include/CppUTest/CppUTestConfig.h \
- /home/vji2i0/TDD/cpputest-3.7.2/include/CppUTest/StandardCLibrary.h \
- /home/vji2i0/TDD/cpputest-3.7.2/include/CppUTest/MemoryLeakDetectorMallocMacros.h \
- /home/vji2i0/TDD/cpputest-3.7.2/include/CppUTest/CommandLineTestRunner.h \
- /home/vji2i0/TDD/cpputest-3.7.2/include/CppUTest/TestHarness.h \
- /home/vji2i0/TDD/cpputest-3.7.2/include/CppUTest/Utest.h \
- /home/vji2i0/TDD/cpputest-3.7.2/include/CppUTest/SimpleString.h \
- /home/vji2i0/TDD/cpputest-3.7.2/include/CppUTest/UtestMacros.h \
- /home/vji2i0/TDD/cpputest-3.7.2/include/CppUTest/TestResult.h \
- /home/vji2i0/TDD/cpputest-3.7.2/include/CppUTest/TestFailure.h \
- /home/vji2i0/TDD/cpputest-3.7.2/include/CppUTest/TestPlugin.h \
- /home/vji2i0/TDD/cpputest-3.7.2/include/CppUTest/MemoryLeakWarningPlugin.h \
- /home/vji2i0/TDD/cpputest-3.7.2/include/CppUTest/MemoryLeakDetectorNewMacros.h \
- /home/vji2i0/TDD/cpputest-3.7.2/include/CppUTest/TestOutput.h \
- /home/vji2i0/TDD/cpputest-3.7.2/include/CppUTest/CommandLineArguments.h \
- /home/vji2i0/TDD/cpputest-3.7.2/include/CppUTest/TestFilter.h
+ ../../../cpputest-3.7.2/include/CppUTest/MemoryLeakDetectorNewMacros.h \
+ ../../../cpputest-3.7.2/include/CppUTest/CppUTestConfig.h \
+ ../../../cpputest-3.7.2/include/CppUTest/StandardCLibrary.h \
+ ../../../cpputest-3.7.2/include/CppUTest/MemoryLeakDetectorMallocMacros.h \
+ ../../../cpputest-3.7.2/include/CppUTest/CommandLineTestRunner.h \
+ ../../../cpputest-3.7.2/include/CppUTest/TestHarness.h \
+ ../../../cpputest-3.7.2/include/CppUTest/CppUTestConfig.h \
+ ../../../cpputest-3.7.2/include/CppUTest/Utest.h \
+ ../../../cpputest-3.7.2/include/CppUTest/SimpleString.h \
+ ../../../cpputest-3.7.2/include/CppUTest/StandardCLibrary.h \
+ ../../../cpputest-3.7.2/include/CppUTest/UtestMacros.h \
+ ../../../cpputest-3.7.2/include/CppUTest/TestResult.h \
+ ../../../cpputest-3.7.2/include/CppUTest/TestFailure.h \
+ ../../../cpputest-3.7.2/include/CppUTest/TestPlugin.h \
+ ../../../cpputest-3.7.2/include/CppUTest/MemoryLeakWarningPlugin.h \
+ ../../../cpputest-3.7.2/include/CppUTest/MemoryLeakDetectorNewMacros.h \
+ ../../../cpputest-3.7.2/include/CppUTest/TestOutput.h \
+ ../../../cpputest-3.7.2/include/CppUTest/CommandLineArguments.h \
+ ../../../cpputest-3.7.2/include/CppUTest/TestFilter.h
 
-/home/vji2i0/TDD/cpputest-3.7.2/include/CppUTest/MemoryLeakDetectorNewMacros.h:
+../../../cpputest-3.7.2/include/CppUTest/MemoryLeakDetectorNewMacros.h:
 
-/home/vji2i0/TDD/cpputest-3.7.2/include/CppUTest/CppUTestConfig.h:
+../../../cpputest-3.7.2/include/CppUTest/CppUTestConfig.h:
 
-/home/vji2i0/TDD/cpputest-3.7.2/include/CppUTest/StandardCLibrary.h:
+../../../cpputest-3.7.2/include/CppUTest/StandardCLibrary.h:
 
-/home/vji2i0/TDD/cpputest-3.7.2/include/CppUTest/MemoryLeakDetectorMallocMacros.h:
+../../../cpputest-3.7.2/include/CppUTest/MemoryLeakDetectorMallocMacros.h:
 
-/home/vji2i0/TDD/cpputest-3.7.2/include/CppUTest/CommandLineTestRunner.h:
+../../../cpputest-3.7.2/include/CppUTest/CommandLineTestRunner.h:
 
-/home/vji2i0/TDD/cpputest-3.7.2/include/CppUTest/TestHarness.h:
+../../../cpputest-3.7.2/include/CppUTest/TestHarness.h:
 
-/home/vji2i0/TDD/cpputest-3.7.2/include/CppUTest/Utest.h:
+../../../cpputest-3.7.2/include/CppUTest/CppUTestConfig.h:
 
-/home/vji2i0/TDD/cpputest-3.7.2/include/CppUTest/SimpleString.h:
+../../../cpputest-3.7.2/include/CppUTest/Utest.h:
 
-/home/vji2i0/TDD/cpputest-3.7.2/include/CppUTest/UtestMacros.h:
+../../../cpputest-3.7.2/include/CppUTest/SimpleString.h:
 
-/home/vji2i0/TDD/cpputest-3.7.2/include/CppUTest/TestResult.h:
+../../../cpputest-3.7.2/include/CppUTest/StandardCLibrary.h:
 
-/home/vji2i0/TDD/cpputest-3.7.2/include/CppUTest/TestFailure.h:
+../../../cpputest-3.7.2/include/CppUTest/UtestMacros.h:
 
-/home/vji2i0/TDD/cpputest-3.7.2/include/CppUTest/TestPlugin.h:
+../../../cpputest-3.7.2/include/CppUTest/TestResult.h:
 
-/home/vji2i0/TDD/cpputest-3.7.2/include/CppUTest/MemoryLeakWarningPlugin.h:
+../../../cpputest-3.7.2/include/CppUTest/TestFailure.h:
 
-/home/vji2i0/TDD/cpputest-3.7.2/include/CppUTest/MemoryLeakDetectorNewMacros.h:
+../../../cpputest-3.7.2/include/CppUTest/TestPlugin.h:
 
-/home/vji2i0/TDD/cpputest-3.7.2/include/CppUTest/TestOutput.h:
+../../../cpputest-3.7.2/include/CppUTest/MemoryLeakWarningPlugin.h:
 
-/home/vji2i0/TDD/cpputest-3.7.2/include/CppUTest/CommandLineArguments.h:
+../../../cpputest-3.7.2/include/CppUTest/MemoryLeakDetectorNewMacros.h:
 
-/home/vji2i0/TDD/cpputest-3.7.2/include/CppUTest/TestFilter.h:
+../../../cpputest-3.7.2/include/CppUTest/TestOutput.h:
+
+../../../cpputest-3.7.2/include/CppUTest/CommandLineArguments.h:
+
+../../../cpputest-3.7.2/include/CppUTest/TestFilter.h:
